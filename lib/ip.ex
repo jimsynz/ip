cc813e68593ebb28eb626a01e36fc59614755410
James Harton <james@rabidtech.co.nz>
2017-10-06 14:56:47 +1300

    FIRST POST

---
+++ lib/ip.ex
@@ -0,0 +1,18 @@
+defmodule IP do
+  @moduledoc """
+  Documentation for IP.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IP.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
